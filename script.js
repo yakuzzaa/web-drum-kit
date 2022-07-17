12e5846b8bbc60e269281d91b53dda11e8c6c6f0
yakuzzaa <chornaya.gryazz@gmail.com>
2022-07-17 16:09:55 +0400

    add click eventListeners,so you can use this shit on your phone, but site adaptation for mobile devices has not even begun

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
-
+function  playOnClick(alpha){
+    const sound = document.querySelector('.sound'+alpha);
+    sound.currentTime = 0;
+    playAudio(sound);
+    L.classList.add('playing');
+    L.classList.remove('playing');
+}
 function playAudio(x) {
     x.play();
 }
@@ -21,3 +27,42 @@ const keys =document.querySelectorAll('.key');
 keys.forEach(key =>key.addEventListener('transitionend',removeTransition));
 
 window.addEventListener('keydown', playSound);
+
+
+//лютейший говнокод, исправлю когда будет вдохновение :))
+const A = document.querySelector('.A');
+A.addEventListener("click",function (e){
+    playOnClick("A");
+});
+const S = document.querySelector('.S');
+S.addEventListener("click",function (e){
+    playOnClick("S");
+});
+const D = document.querySelector('.D');
+D.addEventListener("click",function (e){
+    playOnClick("D");
+});
+const F = document.querySelector('.F');
+F.addEventListener("click",function (e){
+    playOnClick("F");
+});
+const G = document.querySelector('.G');
+G.addEventListener("click",function (e){
+    playOnClick("G");
+});
+const H = document.querySelector('.H');
+H.addEventListener("click",function (e){
+    playOnClick("H");
+});
+const J = document.querySelector('.J');
+J.addEventListener("click",function (e){
+    playOnClick("J");
+});
+const K = document.querySelector('.K');
+K.addEventListener("click",function (e){
+    playOnClick("K");
+});
+const L = document.querySelector('.L');
+L.addEventListener("click",function (e){
+    playOnClick("L");
+});
